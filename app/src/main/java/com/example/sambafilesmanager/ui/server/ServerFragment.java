6521a7a0cc65f85db44e8263653acca49d8cc8f1
Matheus <matheusbelforttorres@gmail.com>
2025-08-16 17:47:06 -0300

    Implemented fake server connection and loading screen

--- app/src/main/java/com/example/sambafilesmanager/ui/server/ServerFragment.java
+++ app/src/main/java/com/example/sambafilesmanager/ui/server/ServerFragment.java
@@ -1,33 +1,67 @@
 package com.example.sambafilesmanager.ui.server;
 
+import static android.view.View.GONE;
+import static android.view.View.VISIBLE;
+
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 import android.widget.EditText;
-import android.widget.TextView;
+import android.widget.FrameLayout;
 
 import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
+import androidx.lifecycle.Observer;
 import androidx.lifecycle.ViewModelProvider;
 
 import com.example.sambafilesmanager.databinding.FragmentServerBinding;
-import com.google.android.material.textfield.TextInputEditText;
 
 public class ServerFragment extends Fragment {
 
     private FragmentServerBinding binding;
+    private ServerViewModel serverViewModel;
+    private EditText ipField;
+    private EditText usernameField;
+    private EditText passwordField;
+    private Button connectButton;
+    private FrameLayout loadingOverlay;
 
     public View onCreateView(@NonNull LayoutInflater inflater,
                              ViewGroup container, Bundle savedInstanceState) {
-        ServerViewModel serverViewModel =
+        serverViewModel =
                 new ViewModelProvider(this).get(ServerViewModel.class);
 
         binding = FragmentServerBinding.inflate(inflater, container, false);
         View root = binding.getRoot();
 
-        final EditText textView = binding.serverIp;
+        ipField = binding.serverIp;
+        usernameField = binding.username;
+        passwordField = binding.password;
+        connectButton = binding.connect;
+        loadingOverlay = binding.loadingOverlay;
+
+        loadingOverlay.setOnTouchListener((v, event) -> true);
+        loadingOverlay.bringToFront();
+
+        serverViewModel.getIsLoading()
+                .observe(getViewLifecycleOwner(), (Observer<Boolean>) this::setLoading);
+
+        connectButton.setOnClickListener((v) -> {
+            serverViewModel.attemptConnection(
+                    ipField.getText().toString(),
+                    usernameField.getText().toString(),
+                    passwordField.getText().toString());
+        });
+
         return root;
+    }
+
+    private void setLoading(boolean loading){
+        loadingOverlay.setVisibility(loading ? VISIBLE : GONE);
+
+        connectButton.setEnabled(!loading);
     }
 
     @Override
--- app/src/main/java/com/example/sambafilesmanager/ui/server/ServerViewModel.java
+++ app/src/main/java/com/example/sambafilesmanager/ui/server/ServerViewModel.java
@@ -5,14 +5,28 @@ import androidx.lifecycle.MutableLiveData;
 import androidx.lifecycle.ViewModel;
 
 public class ServerViewModel extends ViewModel {
-    private final MutableLiveData<String> mText;
+    private final MutableLiveData<Boolean> isLoading = new MutableLiveData<>(false);
 
-    public ServerViewModel() {
-        mText = new MutableLiveData<>();
-        mText.setValue("This is server fragment");
+    public LiveData<Boolean> getIsLoading() {
+        return isLoading;
     }
 
-    public LiveData<String> getText() {
-        return mText;
+    public void attemptConnection(String ipAddress, String username, String password){
+        setIsLoading(true);
+        new Thread(()->{
+            try {
+                Thread.sleep(4000);
+            } catch (InterruptedException e) {
+                throw new RuntimeException(e);
+            }
+            setIsLoading(false);
+        }).start();
+    }
+
+    private void setIsLoading(boolean loading){
+        isLoading.postValue(loading);
+    }
+
+    public ServerViewModel() {
     }
 }
